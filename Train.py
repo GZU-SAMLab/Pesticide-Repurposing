07e9c47775ee18463f812fa11e7edc5698d10df0
dwzhang <2405549856@qq.com>
2024-12-04 16:29:20 +0800

    add train

---
+++ Train.py
@@ -0,0 +1,98 @@
+import os
+import warnings
+
+from openke.config import Tester, Trainer
+from openke.data import TestDataLoader, TrainDataLoader
+from openke.module import model
+from openke.module.loss import SoftplusLoss
+from openke.module.model import ComplEx, RotatE, TransD, TransE, TransH, TransR
+from openke.module.strategy import NegativeSampling
+from utils import get_model, parse_arguments
+
+warnings.filterwarnings("ignore")
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    # data parameters
+    data_path = args.data_path
+    batch_size = args.batch_size
+    threads = args.threads
+    sampling_mode = args.sampling_mode
+    bern_flag = args.bern_flag
+    filter_flag = args.filter_flag
+    neg_ent = args.neg_ent
+    neg_rel = args.neg_rel
+    # model parameters
+    model_name: str = args.model_name
+    dim = args.dim
+    margin = args.margin
+    epsilon = args.epsilon
+
+    # optimizer parameters
+    train_times = args.train_times
+    alpha = args.alpha
+    use_gpu = args.use_gpu
+    opt_method = args.opt_method
+
+    # sampler parameters
+    regul_rate = args.regul_rate
+
+    # Train/Test
+    is_train = args.train
+    is_test = args.test
+    # dataloader for training
+    train_dataloader = TrainDataLoader(
+        in_path=data_path,
+        batch_size=batch_size,
+        threads=threads,
+        sampling_mode=sampling_mode,
+        bern_flag=bern_flag,
+        filter_flag=filter_flag,
+        neg_ent=neg_ent,
+        neg_rel=neg_rel,
+    )
+
+    # dataloader for test
+    test_dataloader = TestDataLoader(data_path, "link")
+
+    # define the model
+    the_model = get_model(
+        model_name.lower(), train_dataloader=train_dataloader, dim=dim, margin=margin, epsilon=epsilon
+    )
+
+    # define sampler and loss function
+    sampler = NegativeSampling(
+        model=the_model,
+        loss=SoftplusLoss(),
+        batch_size=train_dataloader.get_batch_size(),
+        regul_rate=regul_rate,
+    )
+
+    # train the model
+    if is_train:
+        trainer = Trainer(
+            model=sampler,
+            data_loader=train_dataloader,
+            train_times=train_times,
+            alpha=alpha,
+            use_gpu=use_gpu,
+            opt_method=opt_method,
+        )
+        trainer.run()
+
+    # model save
+    if args.save_path:
+        model_save_path = f"{args.save_path}/{model_name}.ckpt"
+        os.makedirs(model_save_path, exist_ok=True)
+        the_model.save_checkpoint(model_save_path)
+
+    # test the model
+    if not is_train and is_test and args.load_path:
+        # test ckpt
+        the_model.load_checkpoint(model_save_path)
+    tester = Tester(model=the_model, data_loader=test_dataloader, use_gpu=use_gpu)
+    mrr, mr, hit10, hit3, hit1, tail_mrr, tail_mr, tail_hit10, tail_hit3, tail_hit1 = tester.run_link_prediction(
+        type_constrain=True
+    )
---
+++ utils.py
@@ -0,0 +1,144 @@
+import argparse
+
+from openke.module.model import ComplEx, RotatE, TransD, TransE, TransH, TransR
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Train/Test a knowledge graph embedding model.")
+
+    # Model parameters
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        choices=["TransE", "Trans", "TransR", "TransD", "ComplEx", "RotatE"],
+        default="TransE",
+        help="The model name. Choices are: TransE, Trans, TransR, TransD, ComplEx, RotatE. Default is TransE.",
+    )
+    parser.add_argument("--dim", type=int, default=200, help="The dimension of the embedding matrix. Default is 200.")
+    parser.add_argument(
+        "--margin",
+        type=int,
+        default=10,
+        help="The margin for loss function. Default is 10. This parameter does not take effect if model_name is ComplEx.",
+    )
+    parser.add_argument(
+        "--epsilon",
+        type=float,
+        default=None,
+        help="The range for initializing the matrix. If None, Xavier initialization is used. Otherwise, uniform initialization in the range [-epsilon, epsilon].",
+    )
+
+    # Data parameters
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        required=True,
+        help="The path to the dataset. Refer to the Openke dataset documentation for dataset format.",
+    )
+
+    # Optimizer parameters
+    parser.add_argument(
+        "--opt_method",
+        type=str,
+        choices=["Adam", "Adadelta", "Adagrad", "SGD"],
+        default="Adam",
+        help="The optimizer method. Choices are: Adam, Adadelta, Adagrad, SGD. Default is Adam.",
+    )
+    parser.add_argument("--alpha", type=float, default=0.01, help="The learning rate. Default is 0.01.")
+    parser.add_argument("--train_times", type=int, default=1000, help="The number of training epochs. Default is 1000.")
+    parser.add_argument("--batch_size", type=int, default=4096, help="The batch size. Default is 4096.")
+    parser.add_argument("--use_gpu", type=bool, default=True, help="Whether to use GPU for training. Default is True.")
+
+    # Sampler parameters
+    parser.add_argument(
+        "--sampling_mode",
+        type=str,
+        choices=["normal", "cross_sampling"],
+        default="normal",
+        help="Sampling mode. 'normal' for head and tail entity sampling together, 'cross_sampling' for head and tail entity cross-sampling. Default is 'normal'.",
+    )
+    parser.add_argument(
+        "--bern_flag",
+        type=int,
+        choices=[0, 1],
+        default=1,
+        help="Negative sample sampling strategy. 1 for Bernoulli sampling, 0 for uniform sampling. Default is 1.",
+    )
+    parser.add_argument(
+        "--filter_flag",
+        type=int,
+        choices=[0, 1],
+        default=1,
+        help="Whether to filter the sampled negative entities. Default is 1.",
+    )
+    parser.add_argument(
+        "--neg_ent", type=int, default=1, help="The ratio of negative samples for entities. Default is 1."
+    )
+    parser.add_argument(
+        "--neg_rel", type=int, default=0, help="The ratio of negative samples for relations. Default is 0."
+    )
+    parser.add_argument("--regul_rate", type=float, default=0, help="The regularization coefficient. Default is 0.")
+
+    parser.add_argument(
+        "--threads", type=int, default=8, help="The num of theads that uesd for train and test. Default is 8"
+    )
+
+    parser.add_argument("--train", type=bool, default=False, help="Train Model ?")
+
+    parser.add_argument("--test", type=bool, default=True, help="Test Model ?")
+
+    parser.add_argument("--save_path", type=str, default="ckpt", help="the path to save checkpoint")
+
+    parser.add_argument("--load_path", type=str, default="", help="the path to loading model")
+    return parser.parse_args()
+
+
+def get_model(model_name: str, train_dataloader, dim, margin, epsilon=None):
+    match model_name.lower():
+        case "transe":
+            the_model = TransE(
+                ent_tot=train_dataloader.get_ent_tot(),
+                rel_tot=train_dataloader.get_rel_tot(),
+                dim=dim,
+                margin=margin,
+                epsilon=epsilon,
+            )
+        case "transr":
+            the_model = TransR(
+                ent_tot=train_dataloader.get_ent_tot(),
+                rel_tot=train_dataloader.get_rel_tot(),
+                dim_e=dim,
+                dim_r=dim,
+                margin=margin,
+            )
+        case "transh":
+            the_model = TransH(
+                ent_tot=train_dataloader.get_ent_tot(),
+                rel_tot=train_dataloader.get_rel_tot(),
+                dim=dim,
+                margin=margin,
+            )
+        case "transd":
+            the_model = TransD(
+                ent_tot=train_dataloader.get_ent_tot(),
+                rel_tot=train_dataloader.get_rel_tot(),
+                dim_e=dim,
+                dim_r=dim,
+                margin=margin,
+                epsilon=epsilon,
+            )
+        case "complex":
+            the_model = ComplEx(
+                ent_tot=train_dataloader.get_ent_tot(),
+                rel_tot=train_dataloader.get_rel_tot(),
+                dim=dim,
+            )
+        case "rotate":
+            the_model = RotatE(
+                ent_tot=train_dataloader.get_ent_tot(),
+                rel_tot=train_dataloader.get_rel_tot(),
+                dim=dim,
+                margin=margin,
+                epsilon=epsilon,
+            )
+    return the_model
